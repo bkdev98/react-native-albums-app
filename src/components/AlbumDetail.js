315561869c4cdd379fd34b28619df2c07ea285fb
Quoc Khanh <bkdev@Quocs-MacBook-Pro.local>
2017-03-10 13:36:51 +0700

    Add album list with card section styled

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,65 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Image, View } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image, image } = album;
+    const {
+        imageStyle,
+        headerTextStyle,
+        thumbnailContainerStyle,
+        thumbnailStyle,
+        headerContentStyle
+    } = styles;
+
     return (
         <Card>
             <CardSection>
-                <Text>{props.album.title}</Text>
+                <View style={thumbnailContainerStyle}>
+                    <Image source={{ uri: thumbnail_image }} style={thumbnailStyle} />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+
+            <CardSection>
+                <Image source={{ uri: image }} style={imageStyle} />
+            </CardSection>
+            
+            <CardSection>
+                <Button />
             </CardSection>
         </Card>
     );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 5,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
 };
 
 export default AlbumDetail;
